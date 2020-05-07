d05c6e3defa20ff9cc453702435a6b1926f72248
microforex <64957418+microforex@users.noreply.github.com>
2020-05-07 19:39:58 +0900

    initial commit.
    練習です

---
+++ index.js
@@ -0,0 +1,8 @@
+
+document.getElementById("baz").addEventListener("click",function(){    
+    /*getでボタンをゲットして、addeventでクリックイベントを追加。functionの中身が{}の中*/
+    alert('ナイスクリック1');
+    alert('ナイスクリック2');
+});
+                
+  
